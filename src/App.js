b59db579d80fdb032d4275d49d8b06cfb0a55a68
kianhaim <kristianhaim@gmail.com>
2021-09-13 20:39:01 -0500

    New Project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Header from "./components/Layout/Header";
+import { Fragment } from "react";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
+
+const Header = props => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
