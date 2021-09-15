abdd0f75bcb885e38b8c6c7323c1ebfff757ce0c
kianhaim <kristianhaim@gmail.com>
2021-09-14 20:00:26 -0500

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 import { Fragment } from "react";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
---
+++ src/components/UI/Card.js
@@ -0,0 +1,7 @@
+import classes from "./Cards.module.css";
+
+const Card = props => {
+  return <div className={classes.card}> {props.children}</div>;
+};
+
+export default Card;
